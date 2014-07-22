fc21cd8738cb071912bd8b813a44f0259d108ac5
Udbhav Gupta <gupta.udbhav@gmail.com>
2014-07-22 14:05:51 -0400

    correct user for git deploys

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,29 +1,34 @@
 execute "git pull" do
   command "git pull origin #{node['chanakya']['git_branch']}"
   cwd node['chanakya']['app_root']
+  user node['chanakya']['user']
 end
 
 # python requirements
 execute "python_requirements" do
   command "env/bin/pip install -r requirements.txt"
   cwd node['chanakya']['app_root']
+  user node['chanakya']['user']
 end
 
 # syncdb and migrations
 execute "syncdb" do
   command "env/bin/python src/manage.py syncdb --noinput"
   cwd node['chanakya']['app_root']
+  user node['chanakya']['user']
 end
 
 execute "migrate" do
   command "env/bin/python src/manage.py migrate"
   cwd node['chanakya']['app_root']
+  user node['chanakya']['user']
 end
 
 # collectstatic
 execute "collectstatic" do
   command "env/bin/python src/manage.py collectstatic --noinput"
   cwd node['chanakya']['app_root']
+  user node['chanakya']['user']
   user node['chanakya']['user']
 end
 
