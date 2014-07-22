4d7d27012cefc6eda0af90fcae33e5c47a598bf1
Udbhav Gupta <gupta.udbhav@gmail.com>
2014-07-22 13:18:02 -0400

    removed old twinsister references

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,30 +1,30 @@
 execute "git pull" do
-  command "git pull origin #{node['twinsister']['git_branch']}"
-  cwd node['twinsister']['app_root']
+  command "git pull origin #{node['chanakya']['git_branch']}"
+  cwd node['chanakya']['app_root']
 end
 
 # python requirements
 execute "python_requirements" do
   command "env/bin/pip install -r requirements.txt"
-  cwd node['twinsister']['app_root']
+  cwd node['chanakya']['app_root']
 end
 
 # syncdb and migrations
 execute "syncdb" do
   command "env/bin/python src/manage.py syncdb --noinput"
-  cwd node['twinsister']['app_root']
+  cwd node['chanakya']['app_root']
 end
 
 execute "migrate" do
   command "env/bin/python src/manage.py migrate"
-  cwd node['twinsister']['app_root']
+  cwd node['chanakya']['app_root']
 end
 
 # collectstatic
 execute "collectstatic" do
   command "env/bin/python src/manage.py collectstatic --noinput"
-  cwd node['twinsister']['app_root']
-  user node['twinsister']['user']
+  cwd node['chanakya']['app_root']
+  user node['chanakya']['user']
 end
 
 # restart gunicorn
