e59a6a6f6c86dd087557cf7e0526308674db5137
Udbhav Gupta <gupta.udbhav@gmail.com>
2014-07-22 15:13:26 -0400

    ugh

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Udbhav Gupta'
 maintainer_email 'dev@udbhavgupta.com'
 description      'Installs/Configures a Django app server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.7'
+version          '0.1.8'
 
 depends "apt"
 depends "locale"
