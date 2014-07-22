907eabac1b0f711ded7063f4e98fb243160c0116
Udbhav Gupta <gupta.udbhav@gmail.com>
2014-07-22 13:54:27 -0400

    missing file

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Udbhav Gupta'
 maintainer_email 'dev@udbhavgupta.com'
 description      'Installs/Configures a Django app server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 depends "apt"
 depends "locale"
